37fcf822aca65eb25eece7bd85a972681ace956d
irfan <im4941668@gmail.com>
2024-01-18 11:39:46 +0530

    initial commit

---
+++ Navigation/NavigationStack.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../Screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const NavigationStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default NavigationStack;
